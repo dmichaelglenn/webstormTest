162bac78407c1a150a2ecd2270a935421704dfdd
Michael Glenn <dmichaelglenn@gmail.com>
2016-10-15 20:00:08 -0700

    added routes; mongoose

--- backEnd/express.js
+++ backEnd/express.js
@@ -1,14 +1,46 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Message = mongoose.model('Message',{
+  msg: String
+});
 
 app.use(bodyParser.json());
 
-app.post('/api/message', function(req, res) {
-  console.log(req.body);
-  res.status(200);
-
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 })
-var server = app.listen(5000, function() {
-  console.log("server listening on ", server.address().port);
+
+app.get('/api/message', GetMessages);
+
+app.post('/api/message', function(req,res){
+  console.log(req.body);
+
+  var message = new Message(req.body);
+
+  message.save();
+
+  res.status(200);
+})
+
+function GetMessages(req, res)
+{
+  Message.find({}).exec(function(err, result){
+    res.send(result);
+  })
+}
+
+mongoose.connect("mongodb://localhost:27017/test", function(err,db){
+  if(!err){
+    console.log("we are connected to mongo");
+
+  }
+})
+
+var server = app.listen(5000, function(){
+  console.log('listening on port ', server.address().port)
 })
